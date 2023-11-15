1fd1060fb3ec698740acd04fd1230346574408dc
skryukov <krjukov_s@mail.ru>
2023-11-15 21:22:39 +0400

    установил preline--

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}', 'node_modules/preline/dist/*.js'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
