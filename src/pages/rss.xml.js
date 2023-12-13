a8a7a17c2b858381c4c5cfbc23e1bc7a60f31b37
skryukov <krjukov_s@mail.ru>
2023-12-13 21:39:54 +0400

    какие-то правки

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/uslugi/${post.slug}/`,
+      link: `/${post.slug}/`,
     })),
   });
 }
